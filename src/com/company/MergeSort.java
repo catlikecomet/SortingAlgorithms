be008b8b1f1738df78cb32095efe8c87e63a99f9
Cameron-Jade Mummery <cami.mummery@aexp.com>
2020-01-27 10:40:17 +0000

    getting a stack overflow error atm line 14

--- src/com/company/MergeSort.java
+++ src/com/company/MergeSort.java
@@ -1,41 +1,59 @@
 package com.company;
 
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MergeSort {
 
-    public void divide(int lowestIndex, int highestIndex, List<String> list, List<String> tempList) {
+    public static void divide(int lowestIndex, int highestIndex, List<Integer> list, List<Integer> tempList) {
 
-        int middleOfList = list.size()/2;
-        divide(lowestIndex, middleOfList, list, tempList);
-        divide(middleOfList + 1, highestIndex, list, tempList);
+        //Using recursion to keep dividing the list until it contains only 1 element at any time
+        if (lowestIndex < highestIndex) {
+            int middleOfList = list.size()/2;
+            divide(lowestIndex, middleOfList, list, tempList);
+            divide(middleOfList + 1, highestIndex, list, tempList);
+        }
+
     }
+
+    public static void print(List<Integer> list) {
+        divide(0, list.size()-1, list, new ArrayList<Integer>(list));
+        System.out.println(list);
+    }
+
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        ArrayList<String> leftList = new ArrayList<>();
-        ArrayList<String> rightList = new ArrayList<>();
-
-        list.add("a");
-        list.add("e");
-        list.add("g");
-        list.add("w");
-        list.add("c");
-        list.add("c");
-        list.add("a");
-
-        int middleOfList = list.size()/2;
-
-        for (int x = 0; x < middleOfList; x++) {
-            leftList.add(list.get(x));
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 9; i++) {
+            list.add(random.nextInt());
         }
-        for (int y = middleOfList; y < list.size(); y++) {
-            rightList.add(list.get(y));
-        }
+        System.out.println(list);
+        print(list);
 
-        System.out.println(leftList);
-        System.out.println(rightList);
+//        ArrayList<String> list = new ArrayList<>();
+//        ArrayList<String> leftList = new ArrayList<>();
+//        ArrayList<String> rightList = new ArrayList<>();
+//
+//        list.add("a");
+//        list.add("e");
+//        list.add("g");
+//        list.add("w");
+//        list.add("c");
+//        list.add("c");
+//        list.add("a");
+//
+//        int middleOfList = list.size()/2;
+//
+//        for (int x = 0; x < middleOfList; x++) {
+//            leftList.add(list.get(x));
+//        }
+//        for (int y = middleOfList; y < list.size(); y++) {
+//            rightList.add(list.get(y));
+//        }
+//
+//        System.out.println(leftList);
+//        System.out.println(rightList);
 
     }
 }
