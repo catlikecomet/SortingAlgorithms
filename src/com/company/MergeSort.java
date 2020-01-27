f427c6ffc9b9b18c656454bc27559df0d94cb111
Cameron-Jade Mummery <cami.mummery@aexp.com>
2020-01-27 14:18:21 +0000

    I am still getting a stack overflow error when using recursion

--- src/com/company/MergeSort.java
+++ src/com/company/MergeSort.java
@@ -10,50 +10,50 @@ public class MergeSort {
 
         //Using recursion to keep dividing the list until it contains only 1 element at any time
         if (lowestIndex < highestIndex) {
-            int middleOfList = list.size()/2;
+            int middleOfList = lowestIndex + (lowestIndex - highestIndex) / 2;
             divide(lowestIndex, middleOfList, list, tempList);
             divide(middleOfList + 1, highestIndex, list, tempList);
+            conquer(lowestIndex, middleOfList, highestIndex, list, tempList);
         }
 
     }
 
+    //sorting and printing the list
     public static void print(List<Integer> list) {
-        divide(0, list.size()-1, list, new ArrayList<Integer>(list));
+        divide(0, list.size() - 1, list, new ArrayList<>(list));
         System.out.println(list);
     }
 
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < 9; i++) {
-            list.add(random.nextInt());
+    public static void conquer(int lowestIndex, int middleOfList, int highestIndex, List<Integer> list, List<Integer> tempList) {
+
+        int leftSide = lowestIndex;
+        int rightSide = middleOfList + 1;
+
+        for (int x = lowestIndex; x <= highestIndex; x++) {
+            tempList.set(x, list.get(x));
         }
-        System.out.println(list);
-        print(list);
 
-//        ArrayList<String> list = new ArrayList<>();
-//        ArrayList<String> leftList = new ArrayList<>();
-//        ArrayList<String> rightList = new ArrayList<>();
-//
-//        list.add("a");
-//        list.add("e");
-//        list.add("g");
-//        list.add("w");
-//        list.add("c");
-//        list.add("c");
-//        list.add("a");
-//
-//        int middleOfList = list.size()/2;
-//
-//        for (int x = 0; x < middleOfList; x++) {
-//            leftList.add(list.get(x));
-//        }
-//        for (int y = middleOfList; y < list.size(); y++) {
-//            rightList.add(list.get(y));
-//        }
-//
-//        System.out.println(leftList);
-//        System.out.println(rightList);
+        //This while loop is comparing the two lists, the ? : operator is similar to an if so if the statement is true it returns the
+        //object to the left of the : if false it returns to the right
+        while (leftSide <= middleOfList && rightSide <= highestIndex) {
+            list.set(lowestIndex++,
+                    tempList.get(leftSide).compareTo(tempList.get(rightSide) < 0
+                            ? tempList.get(leftSide++)
+                            : tempList.get(rightSide++)));
+        }
 
+        while (leftSide <= middleOfList) {
+            list.set(lowestIndex++, tempList.get(leftSide++));
+        }
     }
-}
+
+        public static void main (String[]args){
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < 9; i++) {
+                list.add((int) (Math.random() * 10));
+            }
+            System.out.println(list);
+            print(list);
+        }
+    }
+
