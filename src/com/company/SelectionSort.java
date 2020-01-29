fbd1048c46d908c380ba7cc6c25d4d36f2bcb2be
Cameron-Jade Mummery <cami.mummery@aexp.com>
2020-01-29 11:48:23 +0000

    randomising input

--- src/com/company/SelectionSort.java
+++ src/com/company/SelectionSort.java
@@ -18,13 +18,9 @@ public class SelectionSort {
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(5);
-        list.add(2);
-        list.add(1);
-        list.add(6);
-        list.add(3);
-        list.add(1);
+        for (int i = 0; i < 9; i++) {
+            list.add((int) (Math.random() * 10));
+        }
         System.out.println(list);
         System.out.println(selectionSort(list));
 
