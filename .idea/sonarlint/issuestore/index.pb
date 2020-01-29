da5df28f90f31303894e8bea40e803254302f767
Cameron-Jade Mummery <cami.mummery@aexp.com>
2020-01-29 11:47:37 +0000

    the merge sort and the selection sort are not sorting properly but are doing something strange

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,9 @@
 
 I
 src/com/company/Main.java,4/a/4a831b10439099d44774d0ccc387e808deffaeab
+A
+InsertionSort.iml,8/9/89a5703b8569cdbbcb86ad6f632cfa9395b2a813
+N
+src/com/company/MergeSort.java,4/4/4462e1c792970e5f9026cdf120dfa9d581d2c1a2
+R
+"src/com/company/SelectionSort.java,9/f/9f3e88eb0175bf99f3ae06c2df0814b655931ba4
--- src/com/company/SelectionSort.java
+++ src/com/company/SelectionSort.java
@@ -1,4 +1,34 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class SelectionSort {
+
+    public static ArrayList<Integer> selectionSort(ArrayList<Integer> list) {
+
+        for (int x = 0; x < list.size() -1; x++) {
+                if (list.get(x + 1) < list.get(x)) {
+                    list.set(list.get(x), list.get(x + 1));
+                    list.set(list.get(x + 1), list.get(x));
+                    list.get(x++);
+                }
+            }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(5);
+        list.add(2);
+        list.add(1);
+        list.add(6);
+        list.add(3);
+        list.add(1);
+        System.out.println(list);
+        System.out.println(selectionSort(list));
+
+
+
+    }
 }
